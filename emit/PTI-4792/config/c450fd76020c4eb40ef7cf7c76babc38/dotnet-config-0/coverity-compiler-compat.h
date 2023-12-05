890f19166ec6f7039759de2f375b82376e3af6ac
dave.jamison <dave.jamison@peopletec.com>
2023-12-05 15:20:49 -0600

    coverity

---
+++ emit/PTI-4792/config/c450fd76020c4eb40ef7cf7c76babc38/dotnet-config-0/coverity-compiler-compat.h
@@ -0,0 +1,10 @@
+﻿
+#if __COVERITY__
+#if !defined(COVERITY_COMPAT_4a7b797ab413992fb6cf028be1123fbf)
+#define COVERITY_COMPAT_4a7b797ab413992fb6cf028be1123fbf
+#pragma builtin begin
+
+
+#pragma builtin end
+#endif /* COVERITY_COMPAT_4a7b797ab413992fb6cf028be1123fbf */
+#endif /* __COVERITY__ */
